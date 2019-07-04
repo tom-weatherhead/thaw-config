beaff705a957a71d2a979eb7bdc5aa68036319a6
Tom Weatherhead <tomw@2hrd4u.org>
2019-07-04 08:32:07 -0600

    Dev

--- src/main.js
+++ src/main.js
@@ -19,52 +19,54 @@ function getBabelTargetOptionsFromProfileName (profileName) {
 	}
 }
 
+function getBabelOptions (babelOptionsIn = {}) {
+	// { targetName: foo (or targetOptions: fooOptions), transformClasses: [true | false | undefined] }
+	let pluginsArray = [
+		// ['@babel/plugin-transform-runtime'],
+		// ['@babel/plugin-syntax-dynamic-import']
+	];
+
+	if (babelOptionsIn.transformClasses === true || babelOptionsIn.transformClasses === false) {
+		pluginsArray.push(
+			// From https://babeljs.io/docs/en/babel-plugin-transform-classes :
+			[
+				'@babel/plugin-transform-classes',
+				{
+					loose: babelOptionsIn.transformClasses
+				}
+			]
+		);
+	}
+
+	// "@babel/core": "^7.4.3",
+	// "@babel/plugin-syntax-dynamic-import": "^7.2.0",
+	// "@babel/plugin-transform-classes": "^7.4.3",
+	// "@babel/plugin-transform-runtime": "^7.4.4",
+	// "@babel/preset-env": "^7.4.3",
+	// "@babel/runtime": "^7.4.4",
+	// "babel-loader": "^8.0.5",
+
+	return {
+		presets: [
+			[
+				'@babel/preset-env',
+				{
+					targets: babelOptionsIn.targetOptions || getBabelTargetOptionsFromProfileName(babelOptionsIn.targetProfileName) || {}
+				}
+			]
+		],
+		plugins: pluginsArray //,
+		// comments: false,
+		// sourceType: 'unambiguous'
+	};
+}
+
 // module.exports = (options = {}) => {
 module.exports = {
 	babel: {
 		client: babelTargetOptionsForClientProfile,
 		server: babelTargetOptionsForServerProfile,
-		getOptions: (babelOptionsIn = {}) => {
-			// { targetName: foo (or targetOptions: fooOptions), transformClasses: [true | false | undefined] }
-			let pluginsArray = [
-				// ['@babel/plugin-transform-runtime'],
-				// ['@babel/plugin-syntax-dynamic-import']
-			];
-
-			if (babelOptionsIn.transformClasses === true || babelOptionsIn.transformClasses === false) {
-				pluginsArray.push(
-					// From https://babeljs.io/docs/en/babel-plugin-transform-classes :
-					[
-						'@babel/plugin-transform-classes',
-						{
-							loose: babelOptionsIn.transformClasses
-						}
-					]
-				);
-			}
-
-			// "@babel/core": "^7.4.3",
-			// "@babel/plugin-syntax-dynamic-import": "^7.2.0",
-			// "@babel/plugin-transform-classes": "^7.4.3",
-			// "@babel/plugin-transform-runtime": "^7.4.4",
-			// "@babel/preset-env": "^7.4.3",
-			// "@babel/runtime": "^7.4.4",
-			// "babel-loader": "^8.0.5",
-
-			return {
-				presets: [
-					[
-						'@babel/preset-env',
-						{
-							targets: babelOptionsIn.targetOptions || getBabelTargetOptionsFromProfileName(babelOptionsIn.targetProfileName) || {}
-						}
-					]
-				],
-				plugins: pluginsArray //,
-				// comments: false,
-				// sourceType: 'unambiguous'
-			};
-		}
+		getOptions: getBabelOptions
 	},
 	grunt: {
 		eslint: grunt => {
@@ -115,21 +117,48 @@ module.exports = {
 			grunt.registerTask('test', ['eslint', 'nodeunit']);
 			grunt.registerTask('default', ['test']);
 		},
-		eslint_webpack_concat_nodeunit: dirname => grunt => {
+		// eslint_webpack_concat_nodeunit: dirname => grunt => {
+		eslint_webpack_concat_nodeunit: (options = {}) => grunt => {
+		// eslint_webpack_concat_nodeunit: grunt => {
+			// let options = {};
 			const path = require('path');
+
+			const dirname = options.dirname || '';
+			const forClient = options.forClient;
+			const forServer = options.forServer;
+			const babelOptions = options.babelOptions || {};
+			const nodeunit = typeof options.nodeunit !== 'undefined' ? options.nodeunit : true;
+
+			// const forClient = true;
+			// const forServer = true;
+			const isConcatNeeded = forClient && forServer;
+			// const isConcatNeeded = true;
+
+			console.log('eslint_webpack_concat_nodeunit: options are', options);
+			console.log('eslint_webpack_concat_nodeunit: dirname is', dirname);
+			console.log('eslint_webpack_concat_nodeunit: nodeunit is', nodeunit);
 
 			const packageJsonFilename = 'package.json';
 			const gruntfile = grunt.file.readJSON(packageJsonFilename);
-			const getWebpackConfig = (mode, libraryTarget) => {
-				const babelTargets = libraryTarget === 'commonjs2' ? babelTargetOptionsForServerProfile : babelTargetOptionsForClientProfile;
+			// const getWebpackConfig = (mode, libraryTarget) => {
+			const getWebpackConfig = (mode, targetProfileName) => {
+				console.log(`eslint_webpack_concat_nodeunit: getWebpackConfig(${mode}, ${targetProfileName})`);
+
+				const libraryTarget = targetProfileName === 'client' ? 'global' : targetProfileName === 'server' ? 'commonjs2' : '';
+				// const babelTargets = libraryTarget === 'commonjs2' ? babelTargetOptionsForServerProfile : babelTargetOptionsForClientProfile;
 
 				// const filename = `${ gruntfile.shortName }-webpack-${mode}-${libraryTarget}${filenameSuffix}.js`;
 				const filename = `${ gruntfile.shortName }-${libraryTarget}.js`;
 
+				console.log('eslint_webpack_concat_nodeunit: filename is', filename);
+
+				babelOptions.targetProfileName = targetProfileName;
+
 				return {
 					mode: mode,
 					entry: './src/main.js',
-					target: libraryTarget === 'commonjs2' ? 'node' : undefined, // See https://stackoverflow.com/questions/43915463/webpack-node-js-http-module-http-createserver-is-not-a-function
+					// target: libraryTarget === 'commonjs2' ? 'node' : undefined, // See https://stackoverflow.com/questions/43915463/webpack-node-js-http-module-http-createserver-is-not-a-function
+					target: targetProfileName === 'server' ? 'node' : undefined, // See https://stackoverflow.com/questions/43915463/webpack-node-js-http-module-http-createserver-is-not-a-function
 					output: {
 						path: path.join(dirname, 'dist'), // TODO: Ensure that __dirname has the correct value.
 						filename: filename,
@@ -150,43 +179,10 @@ module.exports = {
 								use: [
 									{
 										loader: 'babel-loader',
-										options: {
-											presets: [
-												[
-													'@babel/preset-env',
-													{
-														// targets: '> 0.25%, not dead'
-														targets: babelTargets
-													}
-												]
-											]
-											// ,
-											// plugins: [
-											// 	'transform-class-properties'
-											// 	? 2019-03-18 : Replace the above with @babel/plugin-proposal-class-properties ?
-											// ]
-
-											// From https://babeljs.io/docs/en/babel-plugin-transform-classes :
-
-											// When extending a native class (e.g., class extends Array {}), the super class needs to be wrapped. This is needed to workaround two problems:
-											//
-											//     Babel transpiles classes using SuperClass.apply(/* ... */), but native classes aren't callable and thus throw in this case.
-											//     Some built-in functions (like Array) always return a new object. Instead of returning it, Babel should treat it as the new this.
-
-											// This plugin allows a class in this package to be used as the base class
-											// of a class outside of this package:
-
-											// $ npm i -D @babel/plugin-transform-classes
-
-											// 'loose' defaults to false:
-											// 'plugins': ['@babel/plugin-transform-classes']
-
-											// 'plugins': [
-											// 	['@babel/plugin-transform-classes', {
-											// 		'loose': true
-											// 	}]
-											// ]
-										}
+										// options: getBabelOptions({
+										// 	targetProfileName: targetProfileName
+										// })
+										options: getBabelOptions(babelOptions)
 									}
 								]
 							}
@@ -201,9 +197,28 @@ module.exports = {
 				};
 			};
 
-			grunt.initConfig({
+			const initGruntConfigOptions = {
 				pkg: gruntfile,
-				concat: {
+				eslint: {
+					target: [
+						'*.js',
+						'src/*.js',
+						'test/*.js'
+					]
+				},
+				nodeunit: {
+					all: ['test/*_test.js']
+				},
+				webpack: {
+					// Possible values for libraryTarget: See https://webpack.js.org/configuration/output/#output-librarytarget :
+					// amd, amd-require, assign, commonjs, commonjs2, global, jsonp, this, umd, var, window.
+					prodserver: getWebpackConfig('production', 'server'),
+					prodclient: getWebpackConfig('production', 'client')
+				}
+			};
+
+			if (isConcatNeeded) {
+				initGruntConfigOptions.concat = {
 					options: {
 						banner: '/**\n' +
 						' * <%= pkg.name %>\n' +
@@ -224,39 +239,62 @@ module.exports = {
 						],
 						dest: 'dist/<%= pkg.shortName %>.js'
 					}
-				},
-				eslint: {
-					target: [
-						'*.js',
-						'src/*.js',
-						'test/*.js'
-					]
-				},
-				nodeunit: {
-					all: ['test/*_test.js']
-				},
-				webpack: {
-					// Possible values for libraryTarget: See https://webpack.js.org/configuration/output/#output-librarytarget :
-					// amd, amd-require, assign, commonjs, commonjs2, global, jsonp, this, umd, var, window.
-					prodcommonjs2: getWebpackConfig('production', 'commonjs2'),
-					prodglobal: getWebpackConfig('production', 'global')
-				}
-			});
+				};
+			}
+
+			grunt.initConfig(initGruntConfigOptions);
 
 			// Tasks
-			grunt.loadNpmTasks('grunt-contrib-concat');
-			grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+			if (isConcatNeeded) {
+				grunt.loadNpmTasks('grunt-contrib-concat');
+			}
+
+			if (nodeunit) {
+				grunt.loadNpmTasks('grunt-contrib-nodeunit');
+			}
+
 			grunt.loadNpmTasks('grunt-eslint');
 			grunt.loadNpmTasks('grunt-webpack');
 
-			// Aliases
-			grunt.registerTask('build', [
-				'webpack:prodcommonjs2',
-				'webpack:prodglobal', // We may want to use prodwindow instead.
+			/*
+			const buildTasks = [
+				'webpack:prodserver',
+				'webpack:prodclient',
 				'concat'
-			]);
-			grunt.registerTask('test', ['eslint', 'nodeunit']);
-			grunt.registerTask('default', ['eslint', 'build', 'nodeunit']);
+			];
+			 */
+			let buildTasks;
+
+			if (isConcatNeeded) {
+				buildTasks = [
+					'webpack:prodserver',
+					'webpack:prodclient',
+					'concat'
+				];
+			} else if (forClient) {
+				buildTasks = ['webpack:prodclient'];
+			} else if (forServer) {
+				buildTasks = ['webpack:prodserver'];
+			} else {
+				buildTasks = [];
+			}
+
+			// Aliases
+			grunt.registerTask('build', buildTasks);
+
+			let testTasks = ['eslint'];
+			let defaultTasks = ['eslint', 'build'];
+
+			if (nodeunit) {
+				const strNodeunit = 'nodeunit';
+
+				testTasks.push(strNodeunit);
+				defaultTasks.push(strNodeunit);
+			}
+
+			grunt.registerTask('test', testTasks);
+			grunt.registerTask('default', defaultTasks);
 		}
 	},
 	version: require('../package.json').version
